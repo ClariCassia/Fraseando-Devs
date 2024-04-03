33e9e02845802ce85741eb9582e02a780f6ec8a4
Clarissa de Cassia <clari.developer@gmail.com>
2024-04-03 16:41:41 -0300

    :recycle: refatoração responsividade

--- src/style.ts
+++ src/style.ts
@@ -1,69 +1,70 @@
-
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Container = styled.div`
-width: 100%;
-min-height: 100vh;
-background-color: #121212;
-display: flex;
-flex-direction: column;
-align-items: center;
-padding-top: 120px;
-@media  (max-width: 480px) {
-  padding: 20px;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #121212;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 120px;
 
-}
-
-img{
-  max-width: 350px; 
   @media (max-width: 480px) {
-    max-width: 280px; 
-  } 
-}
+    padding: 20px;
+  }
 
-h2{
-  color: #fff;
-  margin: 14px 0;  
-  font-weight: 500;
-}
+  img {
+    max-width: 350px;
 
-`
+    @media (max-width: 480px) {
+      max-width: 280px;
+    }
+  }
 
-const CategotyArea = styled.section`
-display: flex;
-gap: 14px;
-margin-bottom: 34px;
+  h2 {
+    color: #fff;
+    margin: 14px 0;
+    font-weight: 500;
+  }
+`;
 
-button{
-  padding: 4px 12px;
-  border-radius: 4px;
-  cursor: pointer; 
-}
+const CategoryArea = styled.section`
+  display: flex;
+  gap: 14px;
+  margin-bottom: 34px;
 
-`
+  button {
+    padding: 4px 12px;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+`;
+
 const PhraseButton = styled.button`
-background-color: #1FA4DB;
-border-radius: 4px;
-padding: 8px 16px;
-color: #fff;
-font-size: 16px;
+  background-color: #1fa4db;
+  border-radius: 4px;
+  padding: 8px 16px;
+  color: #fff;
+  font-size: 16px;
+  cursor: pointer;
+`;
 
-
-`
 const TextSentence = styled.p`
-width: 420px;
-background-color: #fafafa;
-border-radius: 4px;
-text-align: center;
-padding: 14px 22px;
-margin:24px 0 16px 0 ;
-line-height: 150%;
-color:#F57F17;
-font-style: italic;
-font-size: 18px;
-font-weight: bold;
+  width: 420px;
+  background-color: #fafafa;
+  border-radius: 4px;
+  text-align: center;
+  padding: 14px 22px;
+  margin: 24px 0 16px 0;
+  line-height: 150%;
+  color: #f57f17;
+  font-style: italic;
+  font-size: 18px;
+  font-weight: bold;
 
+  @media (max-width: 480px) {
+    width: 330px;
+  }
+`;
 
-`
-
-export { Container, CategotyArea, PhraseButton, TextSentence }
+export { Container, CategoryArea, PhraseButton, TextSentence };
